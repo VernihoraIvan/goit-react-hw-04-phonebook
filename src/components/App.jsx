fd554cb4a491423b07842f68773f2e090059475f
VernihoraIvan <vernihorai@gmail.com>
2023-06-20 12:28:13 +0200

    refactoring to hooks in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import Contacts from './Contacts/Contacts';
 import ContactForm from './ContactForm/ContactForm';
 import Section from './Section/Section';
@@ -7,29 +7,42 @@ import FilterInput from './FilterInput/FilterInput';
 import { nanoid } from 'nanoid';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+const initialContacts =  
+[{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }]
+
+export const App = () => {
+  // state = {
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   filter: '',
+  // };
+
+  const [contacts , setContacts] = useState(initialContacts)
+  const [filter, setFilter] = useState('')
+
+
+
+  const deleteContact = contactId => {
+    setContacts(state => state.filter(contact => contact.id !== contactId))
+    // this.setState(prevState => ({
+    //   contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    // }));
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const handleFilter = event => {
+    setFilter(event.target.value)
+    // this.setState({ filter: event.target.value });
   };
 
-  handleFilter = event => {
-    this.setState({ filter: event.target.value });
-  };
-
-  handleAddContact = ({ name, number }) => {
-    const { contacts } = this.state;
+  const handleAddContact = ({ name, number }) => {
+    // const { contacts } = this.state;
     const newContact = { name, number };
     const isExistingContact = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
@@ -46,6 +59,11 @@ export class App extends Component {
         { name: newContact.name, number: newContact.number, id: nanoid() },
       ],
     }));
+///////////////////////////////////////////////////////////////////////////////////////
+    // setContacts(state => state)
+
+
+
   };
   filteredList = () => {
     const { contacts, filter } = this.state;
@@ -86,16 +104,16 @@ export class App extends Component {
         }}
       >
         <Section title="Phonebook">
-          <ContactForm onSubmit={this.handleAddContact} />
+          <ContactForm onSubmit={handleAddContact} />
         </Section>
         <Section title="Contacts">
           <FilterInput
             value={this.state.filter}
-            handleFilter={this.handleFilter}
+            handleFilter={handleFilter}
           />
           <Contacts
             contacts={filteredContacts}
-            onDeleteContact={this.deleteContact}
+            onDeleteContact={deleteContact}
           />
         </Section>
       </div>
